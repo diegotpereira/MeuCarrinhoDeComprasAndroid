610d1a4a29e62de7e068836fe481e47e135d09ee
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-05-23 21:07:08 -0300

    criação dos arquivos de config

--- app/src/main/java/br/com/meucarrinhodecomprasandroid/MainActivity.java
+++ app/src/main/java/br/com/meucarrinhodecomprasandroid/MainActivity.java
@@ -2,7 +2,14 @@ package br.com.meucarrinhodecomprasandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (MainActivity.this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            MainActivity.this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = Firbase
     }
 }
